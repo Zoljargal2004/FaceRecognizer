d1a8e523ca43900e621e2231ca88cb6457084f59
Zoljargal2004 <zolookorzoloo@gmail.com>
2025-12-16 11:14:02 +0800

    log out

--- web/src/app/(logged-in)/(user)/page.jsx
+++ web/src/app/(logged-in)/(user)/page.jsx
@@ -3,7 +3,7 @@ import { AttendenceWeek } from "components/home/attendence-week";
 import { AvgDuration } from "components/home/avg-duration";
 export default function Page() {
   return (
-    <div className="flex flex-col gap-6">
+    <div className="flex flex-col gap-6 ">
       <div className="space-y-2">
         <h1 className="text-3xl font-bold tracking-tight">
           Тавтай морилно уу!
@@ -13,8 +13,8 @@ export default function Page() {
         </p>
       </div>
 
-      <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3 ">
-        <div className="grid grid-cols-1 gap-6">
+      <div className="">
+        <div className="flex flex-row flex-wrap gap-6">
           <AttendanceToday />
           <AttendenceWeek />
           <AvgDuration />
